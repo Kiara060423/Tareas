1c7d070075a002b00a95121e02edd4aa2bb89f28
Karla Ledo <karlithazaret@gmail.com>
2025-12-17 16:46:12 -0600

    Update canny.py

--- canny.py
+++ canny.py
@@ -1,8 +1,16 @@
 import cv2
-img=cv2.imread('cartas.jpg',0)
-bordeCanny=cv2.Canny(img,100,200)
-cv2.imshow('Original',img)
-cv2.imshow('blur',bordeCanny)
+
+img = cv2.imread('cartas.jpg', cv2.IMREAD_GRAYSCALE)
+
+if img is None:
+    print("Error: No se pudo cargar la imagen 'cartas.jpg'")
+    print("Asegúrate de que el archivo existe en el directorio actual:", cv2.os.getcwd())
+    exit()
+
+borde_canny = cv2.Canny(img, 100, 200)
+
+cv2.imshow('Imagen Original', img)
+cv2.imshow('Bordes Canny', borde_canny)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
