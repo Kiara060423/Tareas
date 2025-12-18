8d06a2fdaf5245a4dbaf3014e86b94a8efcd7ac7
Karla Ledo <karlithazaret@gmail.com>
2025-12-17 20:16:18 -0600

    Update canny.py

--- canny.py
+++ canny.py
@@ -1,16 +1,15 @@
 import cv2
 
-img = cv2.imread('cartas.jpg', cv2.IMREAD_GRAYSCALE)
 
-if img is None:
-    print("Error: No se pudo cargar la imagen 'cartas.jpg'")
-    print("Asegúrate de que el archivo existe en el directorio actual:", cv2.os.getcwd())
-    exit()
+# Cargar la imagen real en la escala de grises
+img = cv2.imread('cartas.jpg',0)
 
-borde_canny = cv2.Canny(img, 100, 200)
+# Detectar bordes con el algoritmo de Canny
+bordeCanny = cv2.Canny(img,100,200)
 
-cv2.imshow('Imagen Original', img)
-cv2.imshow('Bordes Canny', borde_canny)
+#mostrar ventanas
+cv2.imshow('original',img)
+cv2.imshow('canny',bordeCanny)
 
+# Esperar tecla y cerrar
 cv2.waitKey(0)
-cv2.destroyAllWindows()
